bfb74382d4c7f70db96b5dd0e5b6e4ed665ad2f1
Victoria Le <leqvictoria@gmail.com>
2023-02-09 12:38:52 -0500

    disconnection handler

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ dotenv.config()
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO)
+        console.log("Connected to MongoDB.")
     } catch (error) {
         throw error
     }
 }
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected")
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("mongoDB connected")
+})
 
 
 app.listen(8800, () => {
