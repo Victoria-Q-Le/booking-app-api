c1bc0b6057ee143229c864c8ab34ad0ddd7f8383
Victoria Le <leqvictoria@gmail.com>
2023-02-09 14:52:20 -0500

    service model

--- api/models/Service.js
+++ api/models/Service.js
@@ -1,3 +1,10 @@
 import mongoose from 'mongoose'
 
 const {Schema} = mongoose 
+
+const ServiceSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true }
+})
