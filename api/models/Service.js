a70a784eb9be0a95f19cb6111ec5ea0d570610e2
Victoria Le <leqvictoria@gmail.com>
2023-02-10 23:57:39 -0500

    tech

--- api/models/Service.js
+++ api/models/Service.js
@@ -6,5 +6,6 @@ const ServiceSchema = new mongoose.Schema({
     name: { type: String, required: true },
     type: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    tech: {type: String, required: false}
 })
