9a6e2d02c24490b405f548c8a2c005bbba6880fe
Victoria Le <leqvictoria@gmail.com>
2023-02-12 16:23:23 -0500

    error handle

--- api/routes/services.js
+++ api/routes/services.js
@@ -4,7 +4,11 @@ const router = express.Router()
 
 //CREATE
 router.post("/", async (req,res) => {
-
+    try {
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 export default router
