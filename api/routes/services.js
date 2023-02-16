9e24d21c68b7af611f19de14f01df4cb01a49ea8
Victoria Le <leqvictoria@gmail.com>
2023-02-15 22:53:55 -0500

    put request, return new

--- api/routes/services.js
+++ api/routes/services.js
@@ -17,7 +17,7 @@ router.post("/", async (req,res) => {
 //UPDATE
 router.put("/:id", async (req,res) => {
     try {
-        const updatedService = await Service.findByIdAndUpdate(req.params.id, {$set: req.body} )
+        const updatedService = await Service.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true} )
         res.status(200).json(updatedService)
     } catch (error) {
         res.status(500).json(error)
