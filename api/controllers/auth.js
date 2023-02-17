68029cb8c62aa6806b8389b98b4bcdc848b46091
Victoria Le <leqvictoria@gmail.com>
2023-02-17 16:14:29 -0500

    user schema

---
+++ api/controllers/auth.js
@@ -0,0 +1,7 @@
+export const register = async(req, res,next) => {
+    try {
+        const newUser = 
+    } catch (error) {
+        
+    }
+}
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express"
+import { register } from "../controllers/auth.js"
 
 const router = express.Router()
 
-router.get("/", (req,res) => {
-    res.send("This is the first user route")
-})
+router.get("/register", register )
 
 export default router
