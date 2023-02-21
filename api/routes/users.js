8d68ca66e33297c707c9b411d0004522a439728b
Victoria Le <leqvictoria@gmail.com>
2023-02-21 17:51:00 -0500

    check user authentication

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,12 @@
 import express from "express"
 import {updateUser, deleteUser, getUser, getUsers} from "../controllers/user.js"
+import { verifyToken } from "../utils/verifyToken.js"
 
 const router = express.Router()
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("Hello user, you are logged in")
+})
 
 //UPDATE
 router.put("/:id", updateUser)
