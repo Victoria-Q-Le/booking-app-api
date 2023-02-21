b435f57f15572ab1bc7eddd66c77b00791b1a21f
Victoria Le <leqvictoria@gmail.com>
2023-02-21 15:34:52 -0500

    store access token inside a cookie

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from 'bcryptjs'
 import { createError } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const register = async(req, res,next) => {
     try {
@@ -26,7 +27,10 @@ export const login = async(req, res,next) => {
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if (!isPasswordCorrect) return next(createError(400, "Please double check your username and password!"))
-        res.status(200).json(user)
+        const {password, isAdmin, ...otherDetails} = user._doc //because user is stored in ._doc in mongoose db, deconstruct the user to return without the admin and hashed password
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT)//if password is correct, create a new token with the secrect key. JWT: we gonna the hide the user information in the json web token and send it as a cookie, so whenever we make a update service api request, we gonna check if the user isAdmin, if not, return error, if yes, make change to the service
+        res.cookie("access_token", token, {httpOnly: true} ).status(200).json({...otherDetails})
+       
     } catch (error) {
         next(error)
     }
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose"
 import authRoute from "./routes/auth.js"
 import servicesRoute from "./routes/services.js"
 import usersRoute from "./routes/users.js"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -23,6 +24,7 @@ mongoose.connection.on("disconnected", () => {
 })
 
 //MIDDLEWARES
+app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
