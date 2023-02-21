cc87c807cef956e68e59ecf36dce4d4a4d467123
Victoria Le <leqvictoria@gmail.com>
2023-02-21 14:51:58 -0500

    hash password before storing to db

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,14 @@
 import User from "../models/User.js"
+import bcrypt from 'bcryptjs'
 
 export const register = async(req, res,next) => {
     try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
         const newUser = new User ({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hash,
 
         })
         await newUser.save()
@@ -15,6 +18,3 @@ export const register = async(req, res,next) => {
     }
 }
 
-export const getUser = async(req, res, next) => {
-    
-}
