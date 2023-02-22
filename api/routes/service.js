85a05f7a588dd72ed0c28a153542019980840347
Victoria Le <leqvictoria@gmail.com>
2023-02-22 16:58:23 -0500

    service put route

--- api/routes/service.js
+++ api/routes/service.js
@@ -8,8 +8,18 @@ router.post("/", async (req, res) => {
     const newService = new Service (req.body)
 
     try {
-        const savedService = await newService.save()
+        const savedService = await newService.save() 
         res.status(200).json(savedService)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//UPDATE 
+router.put("/:id", async (req,res) => {
+    try {
+        const updatedService = await Service.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true} ) //findByIdAndUpdate will return the original doc not the updated one, therefore to prevent this I added an option new to return the updated doc
+        res.status(200).json(updatedService)
     } catch (error) {
         res.status(500).json(error)
     }
