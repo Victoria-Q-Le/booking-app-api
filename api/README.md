666721afaed0a8113714afb33153bec60336956e
Victoria Le <leqvictoria@gmail.com>
2023-02-24 11:38:32 -0500

    create token

--- api/README.md
+++ api/README.md
@@ -1,1 +1,3 @@
 ###Encoding the Password using bcrypt.js
+ 
+##JsonWebToken: hide all the user infor in the web token and send it as a cookie meaning whenever we sendind a update the server will check the token see whether it is from an Admin and make adjustment accordingly 
--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
-import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
 import {createError} from "../utils/error.js";
 
 
@@ -27,6 +28,7 @@ export const login = async (req,res,next) => {
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password) //comparing the password with the hash - 400 bad request 
         if (!isPasswordCorrect) return next(createError(400, "Please double check your username and password!"))
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT //this the secrect key)//if the password is correct => create a token in this token I chose to save the user id and whether they are admin. Then setting the token inside the cookie (installing cookie Parser )
 
         const {password, isAdmin, ...otherDetails} = user._doc  //deconstruct return user, because didnt want to show all details on client side, also user object is saved under _doc 
 
