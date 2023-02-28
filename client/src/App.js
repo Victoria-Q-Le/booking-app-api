fd3a842343f4452c7168bc35f232266c06badbba
Victoria Le <leqvictoria@gmail.com>
2023-02-27 22:32:02 -0500

    HomePage comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element= 
+        <Route path='/' element = {<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
