93ae203cc1292bbc424cb9092d27bd6dfebe60f7
Victoria Le <leqvictoria@gmail.com>
2023-02-28 18:18:16 -0500

    index page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import HomePage from './pages/HomePage';
+import Index from './pages/Index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element = {<HomePage />} />
+        <Route path='/services' element = {<Index />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      This is the index page 
+    </div>
+  )
+}
+
+export default Index
