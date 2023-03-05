b7b25911782ea5902aa8839bc7767d343fb8608e
Victoria Le <leqvictoria@gmail.com>
2023-03-04 19:19:18 -0500

    header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,7 +3,9 @@ import "./header.css"
 const Header = () => {
   return (
     <div className="header">
-      Header
+      <div className='headerList'>
+        <div className='headerListItem'></div>
+      </div>
     </div>
   )
 }
