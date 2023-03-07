50357de04dd8b518e6ae7b0228bcf95862ab689a
Victoria Le <leqvictoria@gmail.com>
2023-03-07 11:56:54 -0500

    header styling

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,24 @@
 import "./header.css"
+import {faEye, faFaceSmile, faSprayCanSparkles} from "@fortawesome/free-solid-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
 const Header = () => {
   return (
     <div className="header">
-      <div className='headerList'>
-        <div className='headerListItem'>
-          <Font
+      <div className="headerContainer">
+        <div className='headerList'>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faSprayCanSparkles} />
+            <span>Manicures and Pedicures</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faEye} />
+            <span>Lashes</span>
+          </div>
+            <div className='headerListItem'>
+            <FontAwesomeIcon icon={faFaceSmile} />
+            <span>Facial</span>
+          </div>
         </div>
       </div>
     </div>
