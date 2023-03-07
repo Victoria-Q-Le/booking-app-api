b731caffaad686742386e998b562974ea0801016
Victoria Le <leqvictoria@gmail.com>
2023-03-07 12:13:39 -0500

    header title

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -7,19 +7,23 @@ const Header = () => {
     <div className="header">
       <div className="headerContainer">
         <div className='headerList'>
-          <div className='headerListItem'>
+          <div className='headerListItem active'>
             <FontAwesomeIcon icon={faSprayCanSparkles} />
             <span>Manicures and Pedicures</span>
           </div>
+
           <div className='headerListItem'>
             <FontAwesomeIcon icon={faEye} />
             <span>Lashes</span>
           </div>
-            <div className='headerListItem'>
+
+          <div className='headerListItem'>
             <FontAwesomeIcon icon={faFaceSmile} />
             <span>Facial</span>
           </div>
         </div>
+
+        <h1 className="headerTitle">Want to get pampered? Come join us!</h1>
       </div>
     </div>
   )
