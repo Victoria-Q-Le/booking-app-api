042338d71d09a089a9ea60211036d37e2de69735
Victoria Le <leqvictoria@gmail.com>
2023-03-08 09:55:59 -0500

    listmode in index page

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -5,12 +5,12 @@ import { useState } from "react"
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const Header = () => {
+const Header = ({type}) => {
   const [date, setDate] = useState(new Date())
 
   return (
     <div className="header">
-      <div className="headerContainer">
+      <div className={ type === "list" ? "headerContainer listMode" : "headerContainer"}>
         {/* HEADER LIST ITEMS */}
         <div className='headerList'>
           <div className='headerListItem active'>
@@ -29,33 +29,36 @@ const Header = () => {
           </div>
         </div>
 
-        {/* TITLE */}
-        <h1 className="headerTitle">Want to get pampered? Come visit us!</h1>
-        <p className="headerDesc">Come to us with all your beauty needs, from taking care of your nails, to lashes, or just simply a facial to refresh to skin</p>
-        <button className="headerBtn">Login / Register</button>
+        {type !== "list" &&
+        <>
+          {/* TITLE */}
+          <h1 className="headerTitle">Want to get pampered? Come visit us!</h1>
+          <p className="headerDesc">Come to us with all your beauty needs, from taking care of your nails, to lashes, or just simply a facial to refresh to skin</p>
+          <button className="headerBtn">Login / Register</button>
 
-        {/* SEARCH BOX */}
-        <div className="headerSearch">
-          <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faSprayCanSparkles} className="headerIcon" />
-            <input type="text" placeholder="Find by service" className="headerSearchInput"></input>
-          </div>
+          {/* SEARCH BOX */}
+          <div className="headerSearch">
+            <div className="headerSearchItem">
+              <FontAwesomeIcon icon={faSprayCanSparkles} className="headerIcon" />
+              <input type="text" placeholder="Find by service" className="headerSearchInput"></input>
+            </div>
 
-          <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faCalendar} className="headerIcon" />
-            <DatePicker className="date" selected={date} onChange={(date) => setDate(date)} closeOnScroll={true}/>
+            <div className="headerSearchItem">
+              <FontAwesomeIcon icon={faCalendar} className="headerIcon" />
+              <DatePicker className="date" selected={date} onChange={(date) => setDate(date)} closeOnScroll={true}/>
 
-          </div>
-          
-          <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faClock} className="headerIcon" />
-            <span className="headerSearchText">Time</span>
-          </div>
+            </div>
+            
+            <div className="headerSearchItem">
+              <FontAwesomeIcon icon={faClock} className="headerIcon" />
+              <span className="headerSearchText">Time</span>
+            </div>
 
-          <div className="headerSearchItem">
-            <button className="headerBtn">Search</button>
+            <div className="headerSearchItem">
+              <button className="headerBtn">Search</button>
+            </div>
           </div>
-        </div>
+        </>}
 
       </div>
     </div>
--- client/src/pages/index/Index.js
+++ client/src/pages/index/Index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/navBar/Navbar'
+import Header from '../../components/header/Header'
 
 const Index = () => {
   return (
     <div>
       <Navbar />
+      <Header type="list"/>
     </div>
   )
 }
