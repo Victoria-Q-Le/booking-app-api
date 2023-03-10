415b6a0673facadda7cc7ece64d086f82b1a43ed
Victoria Le <leqvictoria@gmail.com>
2023-03-10 13:07:34 -0500

    list services

--- client/src/components/servicesList/servicesList.js
+++ client/src/components/servicesList/servicesList.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import "./servicesList.css"
 import Pedi from "../../assets/images/serviceListImages/pedi.jpg"
+import Mani from "../../assets/images/serviceListImages/mani.jpg"
+import Waxing from "../../assets/images/serviceListImages/waxing.jpg"
+import Facial from "../../assets/images/serviceListImages/facial.jpg"
+import Eyelash from "../../assets/images/serviceListImages/eyelash.jpg"
+
+
+
+
 
 const servicesList = () => {
   return (
@@ -10,6 +18,38 @@ const servicesList = () => {
         <div className='sListTitles'>
           <h1>Pedicures</h1>
           <h2>9 services</h2>
+        </div>
+      </div>
+
+      <div className='sListItem'>
+        <img src={Mani} alt="mani" className='sListImg'/>
+        <div className='sListTitles'>
+          <h1>Manicures</h1>
+          <h2>8 services</h2>
+        </div>
+      </div>
+
+      <div className='sListItem'>
+        <img src={Waxing} alt="Waxing" className='sListImg'/>
+        <div className='sListTitles'>
+          <h1>Waxing</h1>
+          <h2>15 services</h2>
+        </div>
+      </div>
+
+      <div className='sListItem'>
+        <img src={Facial} alt="Facial" className='sListImg'/>
+        <div className='sListTitles'>
+          <h1>Facial</h1>
+          <h2>5 services</h2>
+        </div>
+      </div>
+
+      <div className='sListItem'>
+        <img src={Eyelash} alt="Eyelash" className='sListImg'/>
+        <div className='sListTitles'>
+          <h1>Lash</h1>
+          <h2>8 services</h2>
         </div>
       </div>
     </div>
