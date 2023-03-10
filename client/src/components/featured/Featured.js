db95fac136442695bd4b9f50eca73db10073c0ee
Victoria Le <leqvictoria@gmail.com>
2023-03-10 09:55:24 -0500

    featured images added

--- client/src/components/featured/Featured.js
+++ client/src/components/featured/Featured.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./featured.css"
+import Featured1 from "../../assets/images/featured1.jpg"
 
 const Featured = () => {
   return (
     <div className='featured'> 
       <div className='featuredItem'>
+        <img src={Featured1} alt="featured1" />
       </div>
     </div>
   )
