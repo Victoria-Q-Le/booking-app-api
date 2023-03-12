f42ade85b270b8fc2679deef8d39f78a8fe80fda
Victoria Le <leqvictoria@gmail.com>
2023-03-11 21:42:26 -0500

    staffs

---
+++ client/src/components/staffs/Staffs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Staffs = () => {
+  return (
+    <div>
+      This is the Staffs comp
+    </div>
+  )
+}
+
+export default Staffs
--- client/src/pages/home/HomePage.js
+++ client/src/pages/home/HomePage.js
@@ -2,6 +2,7 @@ import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navBar/Navbar'
 import ServicesList from "../../components/servicesList/servicesList"
+import Staffs from '../../components/staffs/Staffs'
 import './home.css'
 
 const HomePage = () => {
@@ -13,6 +14,8 @@ const HomePage = () => {
         <Featured />
         <h1 className='homeTitle'>Browse by Services</h1>
         <ServicesList />
+        <h1 className='homeTitle'>Meet Our Staffs</h1>
+        <Staffs />
       </div>
     </div>
   )
