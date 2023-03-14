15c580b0c06e5c85ab215ea958bf6bf67e912755
Victoria Le <leqvictoria@gmail.com>
2023-03-14 10:22:36 -0400

    mailing list

--- client/src/components/mailList/MailList.js
+++ client/src/components/mailList/MailList.js
@@ -5,6 +5,10 @@ const MailList = () => {
     <div className="mail">
       <h1 className="mailTitle">Pampering yourself is never so easy</h1>
       <span className="mailDesc">Subcribe to be the first to know about our promotions</span>
+      <div className="mailInputContainer">
+        <input type="text" placeholder="Please Enter Your Email"></input>
+        <button>Subcribe</button>
+      </div>
     </div>
   )
 }
